1a3774cb5b14861072e43ebb0c7eb030f4bee122
Ander <Ander@LAPTOP-HB9PJS6F>
2016-10-12 15:39:38 +0200

    har ændret handler

---
+++ XpTIlbudsapp/VareMedKampanie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XpTIlbudsapp
+{
+    class VareMedKampanie
+    {
+        public int Pris { get; set; }
+        public string Varenavn { get; set; }
+        public string Navn { get; set; }
+        public DateTime Startdate { get; set; }
+        public DateTime SlutDate { get; set; }
+
+        public VareMedKampanie(int pris, string varenavn, string navn, DateTime startdate, DateTime slutDate)
+        {
+            Pris = pris;
+            Varenavn = varenavn;
+            Navn = navn;
+            Startdate = startdate;
+            SlutDate = slutDate;
+        }
+    }
+}
