df168dbd08e7f19e51ac071acdacef104abd93bb
Ander <Ander@LAPTOP-HB9PJS6F>
2016-10-13 11:32:39 +0200

    Har pådateret wiewmodel med inkøbsliste

--- XpTIlbudsapp/ViewModel/ViewModel_OpretTilbud.cs
+++ XpTIlbudsapp/ViewModel/ViewModel_OpretTilbud.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Appointments.AppointmentsProvider;
+using Windows.UI.Popups;
 
 namespace XpTIlbudsapp.ViewModel
 {
@@ -13,11 +14,17 @@ namespace XpTIlbudsapp.ViewModel
 
         public ObservableCollection<VareMedTilbud> Tilbudsvarer { get; set; }
         public ObservableCollection<KampagneMedAltInfo> KampagneVare { get; set; }
+        public static ObservableCollection<VareMedTilbud> Inkøbsliste { get; set; } = new ObservableCollection<VareMedTilbud>();
+
+        public VareMedTilbud selectvare { get; set; }
+        public KampagneMedAltInfo SelectetKampagne { get; set; }
         public RelayCommand SøgKædeCommand { get; set; }
         public RelayCommand SøgKampagneCommand { get; set; }
         public RelayCommand SøgTilbudCommand { get; set; }
         public string Søgeord { get; set; }
-
+        public int TotalIndkøbsliste { get; set; }
+        public RelayCommand AddtilbudtoinkøbslisteCommand { get; set; }
+        public RelayCommand BeregnTotalCommand { get; set; }
         public ViewModel_OpretTilbud()
         {
             Tilbudsvarer = new ObservableCollection<VareMedTilbud>();
@@ -25,6 +32,8 @@ namespace XpTIlbudsapp.ViewModel
             SøgKædeCommand = new RelayCommand(callsøgekæde);
             SøgKampagneCommand = new RelayCommand(callsøgekampagne);
             SøgTilbudCommand = new RelayCommand(callsøgetilbud);
+            AddtilbudtoinkøbslisteCommand = new RelayCommand(addtilbudtoinkøbsliste);
+            BeregnTotalCommand = new RelayCommand(beregntotal);
         }
 
         public void callsøgetilbud()
@@ -56,10 +65,41 @@ namespace XpTIlbudsapp.ViewModel
                 KampagneVare.Add(vareMedTilbud);
             }
         }
+
+        public void addtilbudtoinkøbsliste()
+        {
+            if (selectvare != null)
+            {
+                Inkøbsliste.Add(selectvare);
+            }
+
+            else if (SelectetKampagne != null)
+            {
+                Inkøbsliste.Add(new VareMedTilbud(SelectetKampagne.Pris, SelectetKampagne.Varenavn,
+                    SelectetKampagne.kædenavn, SelectetKampagne.Startdate, SelectetKampagne.SlutDate));
+
+
+            }
+            else
+            {
+                MessageDialog message = new MessageDialog("Vælg venligst vare");
+                message.ShowAsync();
+            }
+        }
+
+        public void beregntotal()
+        {
+            foreach (var Varer in Inkøbsliste)
+            {
+                TotalIndkøbsliste = TotalIndkøbsliste + Varer.Pris;
+
+            }
+        }
     }
-
-
-
-
 }
+
+
+
+
+
 
