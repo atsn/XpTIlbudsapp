687f144ab4ce94cc5a5431e1640b7c8324515e9d
Ander <Ander@LAPTOP-HB9PJS6F>
2016-10-13 10:59:24 +0200

    har lavet wievmodel

--- XpTIlbudsapp/Handler.cs
+++ XpTIlbudsapp/Handler.cs
@@ -38,15 +38,15 @@ namespace XpTIlbudsapp
 
 
                 var tilbudsliste = from s in søgtevare
-                    join t in tildbud on s.Vare_ID equals t.Fk_Vare_ID
-                    select
-                    new {s.Vare_ID, varenavn = s.Navn, t.Fk_Vare_ID, t.Fk_Kaede_ID, t.Pris, t.Slut_Dato, t.Start_Dato};
+                                   join t in tildbud on s.Vare_ID equals t.Fk_Vare_ID
+                                   select
+                                   new { s.Vare_ID, varenavn = s.Navn, t.Fk_Vare_ID, t.Fk_Kaede_ID, t.Pris, t.Slut_Dato, t.Start_Dato };
 
                 // Console.WriteLine(tilbudsliste.First());
                 var tilbudslistemedkæde = from t in tilbudsliste
-                    join k in kæde on t.Fk_Kaede_ID equals k.Kaede_ID
-                    where t.Start_Dato < DateTime.Now && t.Slut_Dato > DateTime.Now
-                    select new VareMedTilbud(t.Pris, t.varenavn, k.Navn, t.Start_Dato, t.Slut_Dato);
+                                          join k in kæde on t.Fk_Kaede_ID equals k.Kaede_ID
+                                          where t.Start_Dato < DateTime.Now && t.Slut_Dato > DateTime.Now
+                                          select new VareMedTilbud(t.Pris, t.varenavn, k.Navn, t.Start_Dato, t.Slut_Dato);
 
                 return tilbudslistemedkæde;
 
@@ -63,7 +63,7 @@ namespace XpTIlbudsapp
             }
 
         }
-        public static IEnumerable<VareMedTilbud> søgeKælde(string søgeord)
+        public static IEnumerable<VareMedTilbud> søgeKæde(string søgeord)
 
         {
 
@@ -93,10 +93,10 @@ namespace XpTIlbudsapp
                                    select new { kædenavn = s.Navn, t.Fk_Vare_ID, t.Fk_Kaede_ID, t.Pris, t.Slut_Dato, t.Start_Dato };
 
                 var tilbudliste2 = from s in tilbudsliste
-                    join v in Vare on s.Fk_Vare_ID equals v.Vare_ID
-                    select new VareMedTilbud(s.Pris, v.Navn, s.kædenavn, s.Start_Dato, s.Slut_Dato);
+                                   join v in Vare on s.Fk_Vare_ID equals v.Vare_ID
+                                   select new VareMedTilbud(s.Pris, v.Navn, s.kædenavn, s.Start_Dato, s.Slut_Dato);
 
-               
+
 
                 return tilbudliste2;
 
@@ -133,37 +133,36 @@ namespace XpTIlbudsapp
             var søgtevare = from k in kampagne where (k.Navn.ToLower().Contains(søgeord)) select k;
 
             var kampanieliste1 = from k in søgtevare
-                join t in kæde on k.Fk_kaede_ID equals t.Kaede_ID
-                select new {k, t};
+                                 join t in kæde on k.Fk_kaede_ID equals t.Kaede_ID
+                                 select new { k, t };
             //Console.WriteLine("1" + kampanieliste1);
             var kampanieliste2 = from k in kampanieliste1
-                join J in Tilbudkampagne on k.k.Kampagne_ID equals J.Fk_Kampagne_ID
-                select new {k, J};
+                                 join J in Tilbudkampagne on k.k.Kampagne_ID equals J.Fk_Kampagne_ID
+                                 select new { k, J };
             //Console.WriteLine("2" + kampanieliste2);
             var kampanieliste3 = from k in kampanieliste2
-                join i in tildbud on k.J.Fk_Tilbud_ID equals i.Tilbud_ID
-                select new {k, i};
+                                 join i in tildbud on k.J.Fk_Tilbud_ID equals i.Tilbud_ID
+                                 select new { k, i };
             //  Console.WriteLine("3" + kampanieliste3);
             var kampanieliste4 = from k in kampanieliste3
-                join v in Vare on k.i.Fk_Vare_ID equals v.Vare_ID
-                where k.i.Start_Dato < DateTime.Now && k.i.Slut_Dato > DateTime.Now
-                select
-                new KampagneMedAltInfo(
+                                 join v in Vare on k.i.Fk_Vare_ID equals v.Vare_ID
+                                 where k.i.Start_Dato < DateTime.Now && k.i.Slut_Dato > DateTime.Now
+                                 select
+                                 new KampagneMedAltInfo(
 
-                    v.Navn,
-                    k.i.Pris,
-                    k.k.k.t.Navn,
-                    k.k.k.k.Navn,
-                    k.i.Start_Dato,
-                    k.i.Slut_Dato);
-                
+                                     v.Navn,
+                                     k.i.Pris,
+                                     k.k.k.t.Navn,
+                                     k.k.k.k.Navn,
+                                     k.i.Start_Dato,
+                                     k.i.Slut_Dato);
 
-          
-                return kampanieliste4;
-               
+
+
+            return kampanieliste4;
+
         }
 
-      
     }
 }
 
--- XpTIlbudsapp/ViewModel/ViewModel_OpretTilbud.cs
+++ XpTIlbudsapp/ViewModel/ViewModel_OpretTilbud.cs
@@ -1,15 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Appointments.AppointmentsProvider;
 
 namespace XpTIlbudsapp.ViewModel
 {
     class ViewModel_OpretTilbud
     {
 
-        
+        public ObservableCollection<VareMedTilbud> Tilbudsvarer  { get; set; }
+        public ObservableCollection<KampagneMedAltInfo> KampagneVare { get; set; }
+        public RelayCommand SøgKædeCommand { get; set; }
+        public RelayCommand SøgKampagneCommand { get; set; }
+        public RelayCommand SøgTilbudCommand { get; set; }
+        public string Søgeord { get; set; }
+
+        public ViewModel_OpretTilbud()
+        {
+            Tilbudsvarer = new ObservableCollection<VareMedTilbud>();
+            KampagneVare = new ObservableCollection<KampagneMedAltInfo>();
+            SøgKædeCommand = new RelayCommand(callsøgekæde);
+            SøgKampagneCommand = new RelayCommand(callsøgekampagne);
+            SøgTilbudCommand = new RelayCommand(callsøgetilbud);
+        }
+
+        public void callsøgetilbud()
+        {
+            IEnumerable<VareMedTilbud> Tilbudsvarerlist = Handler.søgetilbud(Søgeord);
+
+            foreach (var vareMedTilbud in Tilbudsvarerlist)
+            {
+                Tilbudsvarer.Add(vareMedTilbud);
+            }
+        }
+
+        public void callsøgekæde()
+        {
+            IEnumerable<VareMedTilbud> Tilbudsvarerlist = Handler.søgeKæde(Søgeord);
+
+            foreach (var vareMedTilbud in Tilbudsvarerlist)
+            {
+                Tilbudsvarer.Add(vareMedTilbud);
+            }
+        }
+
+        public void callsøgekampagne()
+        {
+            IEnumerable<KampagneMedAltInfo> Tilbudsvarerlist = Handler.søgekampagne(Søgeord);
+
+            foreach (var vareMedTilbud in Tilbudsvarerlist)
+            {
+                KampagneVare.Add(vareMedTilbud);
+            }
+        }
+    }
 
 
 
