f99f98dadf4dddc56565c3985bea6cbe116cb70a
Mads Bjørk Ohmsen <mads696d@edu.easj.dk>
2016-10-13 12:18:40 +0200

    Navigation Færdigt, Ryttet op i projekt

--- XpTIlbudsapp/Pages/Tilbud.xaml.cs
+++ XpTIlbudsapp/Pages/Tilbud.xaml.cs
@@ -20,9 +20,9 @@ namespace XpTIlbudsapp.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Indkoeb : Page
+    public sealed partial class Tilbud : Page
     {
-        public Indkoeb()
+        public Tilbud()
         {
             this.InitializeComponent();
         }
