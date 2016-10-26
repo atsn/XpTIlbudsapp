b59ab27eecc2a79bac902679ff8b7326836cf5ca
Nikol <Nikol@DESKTOP-NRSH0JP>
2016-10-26 11:45:06 +0200

    Notification handler

--- XpTIlbudsapp/Handler/Notification_Handler.cs
+++ XpTIlbudsapp/Handler/Notification_Handler.cs
@@ -2,19 +2,30 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection.Metadata;
+using System.ServiceModel.Dispatcher;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Foundation.Metadata;
+using Windows.System.Threading;
+using Windows.UI.Notifications;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
 
 namespace XpTIlbudsapp
 {
     class Notification_Handler
     {
-        /// </summary>
+        
+        TimeSpan period = TimeSpan.FromHours(1);
+
+
+        private IEnumerable<Vare> Ønskeliste;
         #region 
 
         private static Notification_Handler _instance;
 
-        private IEnumerable<Vare> varelistevirkerikkesomønskeliste;
+       
 
         public static Notification_Handler Instance
         {
@@ -30,15 +41,65 @@ namespace XpTIlbudsapp
 
         #endregion
 
+     
+
+        
+
         public Notification_Handler()
         {
-          
+         
+             notification();
+             
         }
 
 
         public async void notification()
         {
-            varelistevirkerikkesomønskeliste = await Persistency.PersistencyService2.LoadNotesFromJsonAsync();
+            while (true)
+            {
+                Ønskeliste = await Persistency.PersistencyService2.LoadNotesFromJsonAsync();
+
+
+
+                var tilbud = await facade.GetListAsync(new Tilbud());
+
+                var kaede = await facade.GetListAsync(new Kaede());
+
+                var tilbudmedkaede = from s in kaede
+                                     join t in tilbud on s.Kaede_ID equals t.Fk_Kaede_ID
+                                     select
+                                     new { s.Navn, t.Fk_Vare_ID, t.Fk_Kaede_ID, t.Pris, t.Slut_Dato, t.Start_Dato };
+
+
+                foreach (var vare in Ønskeliste)
+                {
+
+                    foreach (var vare1 in tilbudmedkaede)
+                    {
+
+
+                        if (vare1.Fk_Vare_ID.Equals(vare.Vare_ID) && vare.HasBeenNotified.Equals(false))
+                        {
+
+
+
+                            MessageDialog message = new MessageDialog("Vare er nu på tilbud: " + vare.Navn + vare1.Pris + vare1.Navn);
+                            await message.ShowAsync();
+
+                            vare.HasBeenNotified = true;
+
+
+                        }
+
+                    }
+
+                    
+                }
+
+               await Task.Delay(300000);
+            }
+           
+
         }
         
 
